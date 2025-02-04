b62f3ef5c2153f799b8a6065a70d0d77fc92d04f
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-02-04 09:29:59 +0100

    add parent-component in module1

---
+++ src/app/module-1/components/parent-component/parent-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'module-1-components-parent-component',
+  standalone: false,
+  
+  templateUrl: './parent-component.component.html',
+  styleUrl: './parent-component.component.css'
+})
+export class ParentComponentComponent {
+
+}
--- src/app/module-1/module-1.module.ts
+++ src/app/module-1/module-1.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
 import { Component1Component } from './components/component-1/component-1.component';
+import { ParentComponentComponent } from './components/parent-component/parent-component.component';
 
 
 
 @NgModule({
-  declarations: [MainPageComponent, Component1Component],
+  declarations: [MainPageComponent, Component1Component, ParentComponentComponent],
   imports: [
     CommonModule
   ],
