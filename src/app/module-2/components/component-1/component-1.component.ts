98ba07c39f62f2660b5d090020f5ac374623112b
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-02-04 09:12:56 +0100

    add component1 in module2

---
+++ src/app/module-2/components/component-1/component-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'module-2-components-component-1',
+  standalone: false,
+  
+  templateUrl: './component-1.component.html',
+  styleUrl: './component-1.component.css'
+})
+export class Component1Component {
+
+}
--- src/app/module-2/module-2.module.ts
+++ src/app/module-2/module-2.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { Component1Component } from './components/component-1/component-1.component';
 
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, Component1Component],
   imports: [
     CommonModule
   ],
