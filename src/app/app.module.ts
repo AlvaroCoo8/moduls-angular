0942ab8ac03f40fc604b8e966cd0c5307cf2cc1c
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-02-04 09:24:32 +0100

    call module3 in the main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Module1Module } from './module-1/module-1.module';
 import { Module2Module } from './module-2/module-2.module';
+import { Module3Module } from './module-3/module-3.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { Module2Module } from './module-2/module-2.module';
   imports: [
     BrowserModule,
     Module1Module,
-    Module2Module
+    Module2Module,
+    Module3Module
   ],
   providers: [],
   bootstrap: [AppComponent]
