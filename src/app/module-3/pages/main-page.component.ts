1608e6ae1359041e978d365ea946e4d0dd77c76d
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-02-04 09:19:49 +0100

    create main-page module-3

---
+++ src/app/module-3/pages/main-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'module-3-main-page-component',
+  standalone: false,
+
+  templateUrl: 'main-page.component.html',
+})
+export class MainPageComponent {
+  constructor() {}
+}
